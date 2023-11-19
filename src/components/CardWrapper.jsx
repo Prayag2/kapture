76c52a2617a5c361ce2de1e4d16a566b021e9bd4
prayag2 <prayagjain2@gmail.com>
2023-11-19 15:25:46 +0530

    added gitignore

--- src/components/CardWrapper.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CardWrapper = () => {
-  return <div className="flex gap-5 flex-wrap justify-center"></div>;
-};
-
-export default CardWrapper;
--- src/components/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ condition, redirect }) => {
-  return condition ? <Outlet /> : <Navigate to={redirect} />;
-};
-
-export default ProtectedRoute;
